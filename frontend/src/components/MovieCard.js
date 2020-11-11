af8f0f1a4593ca6ba56aabf751bebcf7fc1bbccc
Ranajith Reddy <59986451+ranajithreddy@users.noreply.github.com>
2020-11-12 00:44:36 +0530

    variable cards

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './MovieCard.scss';
-function MovieCard() {
+function MovieCard(card) {
   return (
     <div className="MovieCard">
         <div className="movie_card" id="ave">
           <div className="info_section">
             <div className="movie_header">
-              <img className="locandina" src="https://mr.comingsoon.it/imgdb/locandine/235x336/53715.jpg" />
-              <h1>Black Panther</h1>
-              <h4>2018, Ryan Coogler</h4>
-              <span className="minutes">134 min</span>
-              <p className="type">Action, Adventure, Sci-Fi</p>
+              <img className="locandina" src={card.image} />
+             <h1>{card.title}</h1>
+                <h4>{card.year}, {card.directoe}</h4>
+                <span className="minutes">{card.duration}</span>
+              <p className="type">{card.genre}, {card.genre},{card.genre}</p>
             </div>
             <div className="movie_desc">
               <p className="text">
-                T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake. 
-              </p>
+               {card.description}</p>
             </div>
             <div className="movie_social">
               <ul>
