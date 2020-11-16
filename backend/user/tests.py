e5c2283b20c18f9aeab379247cb035e1edd4cc1f
SBhargav15 <singambhargavram@gmail.com>
2020-11-17 00:47:07 +0530

    add logout test

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -28,6 +28,8 @@ class UrlTest(SimpleTestCase):
         
 class ApiTest(APITestCase):
 
+    token = None
+
     def test_registration(self):
         data={"username":"testcase", "email":"test@localhost.app", "password":"some_strong_psw"}
         response=self.client.post("/users/register",data,format="json")
@@ -37,14 +39,11 @@ class ApiTest(APITestCase):
         self.test_registration()
         data={"username":"testcase","password":"some_strong_psw"}
         response=self.client.post("/users/token-auth/login",data,format="json")
+        self.token=response.data['token']
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-
-    
-    
-
-
-    
-
-
-
+    def test_logout(self):
+        self.test_login()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token) 
+        response=self.client.delete("/users/token-auth/logout")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
