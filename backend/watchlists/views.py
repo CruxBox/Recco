772190e45a21ee8fec139eddd099e8056f99e23b
SBhargav15 <singambhargavram@gmail.com>
2020-11-17 13:23:14 +0530

    basic watchlist crud

--- backend/watchlists/views.py
+++ backend/watchlists/views.py
@@ -11,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 from .models import *
+from movies.models import * 
 from .serializers import *
 
 
@@ -30,15 +31,66 @@ def get_watchlists(request):
         return Response(response.data,status=status.HTTP_201_CREATED)
 
 
-
-
-@api_view(('GET',))
+@api_view(('DELETE','GET','PUT'))
 @authentication_classes((TokenAuthentication, ))
 def get_watchlist_details(request, id):
     if request.method == "GET":
         try:
             watchlists = Watchlist.objects.get(id=id)
             response = WatchlistsSerializer(watchlists)
+            return Response(response.data,status=status.HTTP_200_OK)
+        except ObjectDoesNotExist as e:
+            return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    elif request.method == "DELETE":
+        try:
+            print("Ok")
+            watchlist = Watchlist.objects.get(id=id)
+            watchlist.delete()
+            return Response({"message":"Succesfully Deleted"},status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist as e:
+            return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    
+    elif request.method == "PUT":
+        try:
+            data = request.data
+            watchlist = Watchlist.objects.get(id=id)
+            if 'name' in data:
+                watchlist.name=data['name']
+            if 'description' in data:
+                watchlist.description=data['description']
+            watchlist.save()
+            response = WatchlistsSerializer(watchlist)
+            return Response(response.data,status=status.HTTP_200_OK)
+        except ObjectDoesNotExist as e:
+            return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    
+
+@api_view(('POST',))
+@authentication_classes((TokenAuthentication, ))
+def add_to_watchlist(request, id):
+    if request.method == "POST":
+        try:
+            data = request.data
+            watchlist = Watchlist.objects.get(id=id)
+            for movie_id in data['movies']:
+                movie = Movie.objects.get(tmdb_id=movie_id['tmdb_id'])
+                watchlist.movies.add(movie)
+            response = WatchlistsSerializer(watchlist)
+            return Response(response.data,status=status.HTTP_201_CREATED)
+        except ObjectDoesNotExist as e:
+            return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(('DELETE',))
+@authentication_classes((TokenAuthentication, ))
+def remove_from_watchlist(request, id):
+    if request.method == "DELETE":
+        try:
+            data = request.data
+            watchlist = Watchlist.objects.get(id=id)
+            for movie_id in data['movies']:
+                movie = Movie.objects.get(tmdb_id=movie_id['tmdb_id'])
+                watchlist.movies.remove(movie)
+            response = WatchlistsSerializer(watchlist)
             return Response(response.data,status=status.HTTP_200_OK)
         except ObjectDoesNotExist as e:
             return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
