e55ca0185c98964af8410753cf21329065b5c6a1
SBhargav15 <singambhargavram@gmail.com>
2020-12-12 00:50:40 +0530

    resolves #23

--- frontend/src/components/Watchlist.js
+++ frontend/src/components/Watchlist.js
@@ -1,88 +1,77 @@
-import React from 'react';
-import './Watchlist.css';
+import React, { useEffect, useState } from "react";
+import { UserContext } from "./Auth/userContext";
+import axios from "axios";
+import Card from "./card";
+import { toast } from "react-toastify";
+import "./Watchlist.css";
 function Watchlist() {
+  const { user } = React.useContext(UserContext);
+  const [owned, setOwned] = useState([]);
+  const [shared, setShared] = useState([]);
+
+  useEffect(() => {
+    function get_details() {
+      var info = JSON.parse(localStorage.getItem("user"));
+      console.log(info.user.favourite);
+      var config = {
+        method: "get",
+        url: "http://127.0.0.1:8000/watchlists/",
+        headers: {
+          Authorization: `Token ${info.token}`,
+          "Content-Type": "application/json",
+        },
+      };
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          let data = response.data;
+          let owned = data.owned.movies;
+          let shared = data.shared.movies;
+          console.log(owned, data, shared);
+          setOwned(data.owned);
+          setShared(data.shared);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    get_details();
+  }, []);
+
   return (
-        <div className="Watchlist">
-        <div class="row1"><h2>Owned Watchlist</h2>
-        <div class="row2"><h3>Watchlist 1</h3>
-        <h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-       
-        </div>
-        </div>
-<div class="row2"><h3>Watchlist2</h3>
-<h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        
-        </div>
-
-        <div class="row3"><h2>Shared Watchlist</h2>
-        <div class="row4"><h3>Watchlist 3</h3>
-        <h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-       
-        </div>
-        </div>
-<div class="row4"><h3>Watchlist4</h3>
-<h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-        <div className="card">
-        </div>
-       
-        </div>
-
-
+    <div className="Watchlist">
+      <div class="row1">
+        <h2>Owned Watchlists</h2>
+        {owned.map((watch, i) => {
+          return (
+            <div class="row2" key={i}>
+              <h3>{watch.name}</h3>
+              <h5>{watch.description}</h5>
+              {watch.movies.map((movie, id) => {
+                return <Card key={id} id={movie.tmdb_id} />;
+              })}
             </div>
+          );
+        })}
+      </div>
+
+      <div class="row3">
+        <h2>Shared Watchlists</h2>
+        {shared &&
+          shared.map((watch, i) => {
+            return (
+              <div class="row2" key={i}>
+                <h3>{watch.name}</h3>
+                <h5>{watch.description}</h5>
+                {watch.movies.map((movie, id) => {
+                  return <Card key={id} id={movie.tmdb_id} />;
+                })}
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/components/card.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { UserContext } from "./Auth/userContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "./Watchlist.css";
+import requests from "./requests";
+import { useHistory } from "react-router-dom";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+export default function Card({ id }) {
+  const [movie, setMovie] = useState("");
+  const history = useHistory();
+  useEffect(() => {
+    function get_movie_details(id) {
+      var config = {
+        method: "get",
+        url: `http://127.0.0.1:8000/movies/${id}/`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          setMovie(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    get_movie_details(id);
+  }, []);
+
+  return (
+    <div className="card">
+      <img
+        key={movie.id}
+        onClick={() => history.push(`/movie/${movie.id}`)}
+        className={`row_poster`}
+        src={`${base_url}${movie.poster_path}`}
+        alt={movie.name}
+      />
+    </div>
+  );
+}
