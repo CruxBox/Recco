d489431237a12cb983d43ca09b45b8e9ee8e06bb
Ranajith Reddy <59986451+ranajithreddy@users.noreply.github.com>
2020-12-11 23:24:49 +0530

    watchlist

---
+++ frontend/src/components/Watchlist.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './Watchlist.css';
+function Watchlist() {
+  return (
+        <div className="Watchlist">
+        <div class="row1"><h2>Owned Watchlist</h2>
+        <div class="row2"><h3>Watchlist 1</h3>
+        <h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+       
+        </div>
+        </div>
+<div class="row2"><h3>Watchlist2</h3>
+<h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        
+        </div>
+
+        <div class="row3"><h2>Shared Watchlist</h2>
+        <div class="row4"><h3>Watchlist 3</h3>
+        <h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+       
+        </div>
+        </div>
+<div class="row4"><h3>Watchlist4</h3>
+<h5>asdfg asdfghjl asfghjkk agvajaknaknaknkan</h5>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+        <div className="card">
+        </div>
+       
+        </div>
+
+
+            </div>
+  );
+}
+
+export default Watchlist;
